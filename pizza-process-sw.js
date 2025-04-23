508a4b8af8a91c6727b32afad591c14a5a477cbe
stevegiunta <steve.4gs@gmail.com>
2025-04-23 15:28:55 -0400

    Update

--- pizza-process-sw.js
+++ pizza-process-sw.js
@@ -1,43 +1,43 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('pizza-process-v1').then((cache) => {
-      return cache.addAll(['manifest.json']);
-    })
-  );
-  self.skipWaiting();
-});
+    event.waitUntil(
+      caches.open('pizza-process-v1').then((cache) => {
+        return cache.addAll(['/pizza-process/manifest.json']);
+      })
+    );
+    self.skipWaiting();
+  });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.filter((name) => name !== 'pizza-process-v1')
-          .map((name) => caches.delete(name))
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.filter((name) => name !== 'pizza-process-v1')
+            .map((name) => caches.delete(name))
+        );
+      })
+    );
+    self.clients.claim();
+  });
+
+  self.addEventListener('fetch', (event) => {
+    const url = new URL(event.request.url);
+
+    if (url.pathname.endsWith('pizza_process.html')) {
+      event.respondWith(
+        fetch(event.request).catch(() => {
+          return caches.match(event.request);
+        })
       );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
-
-  if (url.pathname.endsWith('pizza_process.html')) {
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match(event.request);
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request).then((networkResponse) => {
-          return caches.open('pizza-process-v1').then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
+    } else {
+      event.respondWith(
+        caches.match(event.request).then((response) => {
+          return response || fetch(event.request).then((networkResponse) => {
+            return caches.open('pizza-process-v1').then((cache) => {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
+            });
           });
-        });
-      })
-    );
-  }
-});
+        })
+      );
+    }
+  });
